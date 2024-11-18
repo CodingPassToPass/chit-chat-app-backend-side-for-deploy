157c607c9ba1ffa9c7c767ff9699ea5fdde273e5
CodingPassToPass <codingiron24103@gmail.com>
2024-11-18 16:21:02 +0530

    new features

--- controllers/user.js
+++ controllers/user.js
@@ -47,10 +47,9 @@ const newUser =  catchAsyncErrors( async ( req, res, next)=>{
         }
     });
 
-    
 
     sendToken( res, user, 201, "User created successfully");
-    
+
 });
 
 //login user and save token in cookies
@@ -183,7 +182,7 @@ const acceptFriendRequest = catchAsyncErrors( async( req, res, next)=>{
     const { requestId, accept} = req.body;
     
     
-    const request = await Request.findById(requestId).populate("receiver","name _id").populate("sender","name");
+    const request = await Request.findById(requestId).populate("receiver","name _id").populate("sender","name _id");
     
     if(!request){
         return next( new errorHandler("Request Not Found",404));
@@ -229,7 +228,7 @@ const acceptFriendRequest = catchAsyncErrors( async( req, res, next)=>{
     })
 
     //refetch chats
-    emitEvent( req, REFETCH_CHATS, members, "refetch all chats");
+    emitEvent( req, REFETCH_CHATS, members, { message:"friendRequestAccept", sender: { _id: request.sender._id, name: request.sender.name}, receiver:{ _id: request.receiver._id, name: request.receiver.name} });
         
 })
 
@@ -302,8 +301,3 @@ const getMyFriends = catchAsyncErrors( async( req, res ,next)=>{
 })
 
 export { login, newUser, getMyProfile, logout, searchUser, sendFriendRequest, acceptFriendRequest, getAllNotifications, getMyFriends};
-
-
-
-
-
